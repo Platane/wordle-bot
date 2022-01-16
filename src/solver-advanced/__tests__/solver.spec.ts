d83fd5b8265a000d1408c9948413ae2810af11a3
platane <me@platane.me>
2022-01-16 21:08:59 +0100

    🚀 add solver test for advanced solver

---
+++ src/solver-advanced/__tests__/solver.spec.ts
@@ -0,0 +1,32 @@
+import { createSolver } from "../index";
+import { Line, lineToString } from "../../type";
+import { getWordList } from "../../wordlist";
+import { pickRandom } from "../../utils.array";
+import { mockMathRandom } from "../../utils-pseudoRandom";
+import { evaluateWord, isLineCorrect } from "../../solver-simple";
+
+it("solver", async () => {
+  const words = await getWordList();
+
+  mockMathRandom(9987);
+
+  const solution = pickRandom(words);
+
+  const solver = createSolver(words);
+
+  const lines: Line[] = [];
+
+  for (let i = 10; i--; ) {
+    const w = solver.getNextWord();
+
+    const line = evaluateWord(solution, w!);
+
+    solver.reportLine(line);
+
+    lines.push(line);
+  }
+
+  console.log(solution + "\n" + lines.map(lineToString).join("\n"));
+
+  expect(lines.some(isLineCorrect)).toBeTruthy();
+});

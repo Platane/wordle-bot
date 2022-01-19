3757f83c13994e522ac8887e9d2967c12ce06bb1
platane <me@platane.me>
2022-01-19 12:06:40 +0100

    🚀 tweet link to media player (to avoid mp4 without mimetype)

--- src/twitter/client.ts
+++ src/twitter/client.ts
@@ -6,12 +6,12 @@ export const getTwitterClient = () => {
   return twitterClient;
 };
 
-export const tweetResult = async (sharedText: string, recordingUrl: string) => {
+export const tweetResult = async (sharedText: string, recordUrl: string) => {
   const twitterClient = getTwitterClient();
   const { data: firstTweet } = await twitterClient.v2.tweet(sharedText);
   // const mediaId = await twitterClient.v1.uploadMedia(res.recordFile);
   await twitterClient.v2.tweet({
-    text: `SPOILER: ${recordingUrl}`,
+    text: `SPOILER: https://platane.github.io/wordle-bot/media-player.html?videoUrl=${recordUrl}`,
     // media: { media_ids: [mediaId] },
     reply: { in_reply_to_tweet_id: firstTweet.id },
   });
